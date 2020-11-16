239a6770787e3343a07d6ae7e92af05f2df6a216
archanabotla08 <archana.botla@gmail.com>
2020-11-16 16:47:42 +0530

    [Archana] Added: UC2 to calculate the total fare for multiple rides

--- src/main/java/com/blz/cabinvoicegeneratortest/InvoiceGenerator.java
+++ src/main/java/com/blz/cabinvoicegeneratortest/InvoiceGenerator.java
@@ -13,5 +13,12 @@ public class InvoiceGenerator {
 		}
 		return cost;
 	}
+	public double calculateFare(Ride[] rides) {
+		 double	totalFare  = 0;
+		for(Ride ride: rides) {
+		 	totalFare += this.calculateFare(ride.distance,ride.time);
+		}
+		return totalFare;
+	}
 	
 }
--- src/test/java/com/blz/cabinvoicegeneratortest/InvoiceServiceTest.java
+++ src/test/java/com/blz/cabinvoicegeneratortest/InvoiceServiceTest.java
@@ -22,5 +22,24 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(25, fare,0.0);
 	}
+	@Test
+	public void givenDistanceTime_SHouldReturnMininumFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(5.0, fare,0.0);
+	}
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare() {
+		Ride[] rides = {
+				new Ride(2.0,5),
+				new Ride(0.1,1)
+		};
+		
+		double fare = invoiceGenerator.calculateFare(rides);
+		assertEquals(30.0, fare,0.0);
+	}
+	
 
 }
